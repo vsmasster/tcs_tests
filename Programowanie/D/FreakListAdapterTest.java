761cd0f40145fbde6021fe712770c1c6ba5079c5
Michał Glapa <michal.glapa@gmail.com>
2015-03-27 19:39:41 +0100

    [Programowanie][D] testy

---
+++ Programowanie/D/FreakListAdapterTest.java
@@ -0,0 +1,87 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class FreakListAdapterTest {
+
+    @Test
+    public void testExample() throws Exception {
+        FreakList<Integer> Cycle11 = new FreakList<Integer> () {
+            private LinkedList<Integer> ttt = new LinkedList<Integer>();
+            {
+                int a = 2;
+                for (int i = 0; i < 10; i++)
+                {
+                    ttt.add(a);
+                    a = (2*a)%11;
+                }
+            }
+            @Override
+            public Integer pop() throws AccessDenied, WasWrong, IsEmpty {
+                if (ttt.isEmpty())
+                    throw new IsEmpty();
+                return ttt.pop();
+            }
+        };
+
+        FreakListAdapter<Integer> f = new FreakListAdapter<Integer>(Cycle11);
+
+        List<Integer> expectedResults = Arrays.asList(2,4,8,5,10,9,7,3,6,1);
+
+        for(Integer i : expectedResults) {
+            try {
+                assertEquals(i, f.pop());
+            }
+            catch (IsEmpty e) {
+                fail();
+            }
+        }
+        try {
+            f.pop();
+            fail();
+        }
+        catch (IsEmpty e) {}
+    }
+    @Test
+    public void testWrongValues() throws Exception{
+        FreakList<Integer> fl = new FreakList<Integer>() {
+            int counter = 0;
+            @Override
+            public Integer pop() throws AccessDenied, WasWrong, IsEmpty {
+                switch(++counter) {
+                    case 1 :
+                        return 2;
+                    case 2 :
+                        throw new WasWrong();
+                    case 3 :
+                        return 1;
+                    case 4 :
+                        return 3;
+                    default:
+                        throw new IsEmpty();
+                }
+            }
+        };
+        FreakListAdapter<Integer> f = new FreakListAdapter<Integer>(fl);
+
+        List<Integer> expectedResults = Arrays.asList(1,2,3);
+
+        for(Integer i : expectedResults) {
+            try {
+                assertEquals(i, f.pop());
+            }
+            catch (IsEmpty e) {
+                fail();
+            }
+        }
+        try {
+            f.pop();
+            fail();
+        }
+        catch (IsEmpty e) {}
+    }
+}
